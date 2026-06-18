8e52d545f5b4f5ba7a761e8f9c58bd606da8a08d
Kyrylo <danilin2508@gmail.com>
2026-06-18 13:43:51 +0300

    feat: add frontend types - IOrder, IProduct, IPrice, IGuarantee

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,1 +1,32 @@
-export {}
+export interface IGuarantee {
+  start: string;
+  end: string;
+}
+
+export interface IPrice {
+  value: number;
+  symbol: string;
+  isDefault: number;
+}
+
+export interface IProduct {
+  id: number;
+  serialNumber: number;
+  isNew: number;
+  photo: string;
+  title: string;
+  type: string;
+  specification: string;
+  guarantee: IGuarantee;
+  price: IPrice[];
+  order: number;
+  date: string;
+}
+
+export interface IOrder {
+  id: number;
+  title: string;
+  date: string;
+  description: string;
+  products?: IProduct[];
+}
