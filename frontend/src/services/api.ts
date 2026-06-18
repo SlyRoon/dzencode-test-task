ef97fc6572e193b33634d7d473bc7e660304bb96
Kyrylo <danilin2508@gmail.com>
2026-06-18 14:01:59 +0300

    feat: add api service - getOrders, getProducts, deleteOrder

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,1 +1,19 @@
-export {}
+import axios from 'axios';
+import type { IOrder, IProduct } from '../types';
+const api = axios.create({
+  baseURL: 'http://localhost:3000/api'
+});
+ 
+
+
+export const getOrders = async (): Promise<IOrder[]> => {
+    const {data} = await api.get<IOrder[]>('/orders');
+    return data
+}
+export const deleteOrder = async (id: number): Promise<void> => {
+    await api.delete(`/orders/${id}`)
+}
+export const getProducts = async (): Promise<IProduct[]> => {
+    const {data} = await api.get<IProduct[]>('/products');
+    return data
+}
