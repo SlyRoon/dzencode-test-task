3697300b66387c90509b7b4df442e4e1669b4017
Kyrylo <danilin2508@gmail.com>
2026-06-19 10:51:55 +0300

    feat(router): setup base application routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Navigate to='/orders' replace />}/>
+          <Route path='orders' element={<Orders/>}/>
+          <Route path='products' element={<Products/>}/>
+        </Route>
+        <Route
+        path='*'
+        element={<div>404 Not Found</div>}
+        />
+
+        
+      </Routes>
+    </BrowserRouter>
   )
 }
 
