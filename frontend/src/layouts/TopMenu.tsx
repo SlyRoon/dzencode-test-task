c787358f3096b2d5e3203e8c52e09dd387f595f8
Kyrylo <danilin2508@gmail.com>
2026-06-19 12:13:09 +0300

    feat(currentTime): add currentTime in TopMenu

--- frontend/src/layouts/TopMenu.tsx
+++ frontend/src/layouts/TopMenu.tsx
@@ -1,3 +1,19 @@
-const TopMenu = () => { return <div></div> }
+import { useEffect,useState } from "react"
+function TopMenu(){
+    const [currentTime, setCurrentTime] = useState(new Date())
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentTime(new Date())
+
+        },1000)
+        return () => {
+            clearInterval(interval)
+        }
+    },[])
+    return (
+        <div>{currentTime.toLocaleTimeString()}</div>
+    )
+
+}
 
 export default TopMenu
