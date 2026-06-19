76c94a3664e46248a4c24dcfad7c18ae5a8b7e76
Kyrylo <danilin2508@gmail.com>
2026-06-19 11:28:05 +0300

    feat(navigate): add navigate sidebar

--- frontend/src/layouts/Sidebar.tsx
+++ frontend/src/layouts/Sidebar.tsx
@@ -1,3 +1,11 @@
-const Sidebar = () => { return <div></div> }
+import { NavLink } from "react-router-dom"
 
+function Sidebar() {
+    return (
+        <div>
+            <NavLink to={'/orders'}>OrderPage </NavLink>
+            <NavLink to={'/products'}>ProductsPage</NavLink>
+        </div>
+    )
+}
 export default Sidebar
