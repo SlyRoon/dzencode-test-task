7ef7e81e4f6acfa026dddbaedc24c3a27dd83fe4
Kyrylo <danilin2508@gmail.com>
2026-06-29 19:40:08 +0300

    Proxy frontend API through nginx

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { IOrder, IProduct } from '../types';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: '/api',
 });
 
 // ----- Orders -----
