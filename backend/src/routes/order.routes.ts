77e71b9c40b0d56f8df99d69f632b0924b56f9f8
Kyrylo <danilin2508@gmail.com>
2026-06-30 13:04:03 +0300

    test actions

--- backend/src/routes/order.routes.ts
+++ backend/src/routes/order.routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { orderController } from '../controllers/order.controller';
 const router = Router();
 
+
 router.get('/', orderController.getAll);
 router.post('/', orderController.create);
 router.delete('/:id', orderController.deleteById);
